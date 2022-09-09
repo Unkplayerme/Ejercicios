2c8a64eb8b63264540ee3c3337d0a9db8e745058
Unkplayerme <unkplayerme@gmail.com>
2022-09-09 12:14:55 -0500

    Final

--- Tema 9/src/App.java
+++ Tema 9/src/App.java
@@ -1,79 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Cliente miCliente = new Cliente();
+        Cliente cliente = new Cliente();
 
-        miCliente.setEdad(21);
-        miCliente.setNombre("Lorka");
-        miCliente.setTelefono("6184763517");
-        miCliente.setCredito(1000);
+        cliente.edad = 21;
+        cliente.nombre = "Lorka";
+        cliente.telefono = "6184763517";
+        cliente.credito = 1000;
 
-        System.out.println("Edad: " + miCliente.getEdad() + "\nNombre: " + miCliente.getNombre() + "\nTeléfono: " + miCliente.getTelefono() + "\nCrédito: " + miCliente.getCredito());
+        System.out.println("Nombre: " + cliente.nombre + "\nEdad: "  + cliente.edad  + "\nTeléfono: " + cliente.telefono + "\nCrédito: " + "$" + cliente.credito);
         
-        Trabajador miTrabajador = new Trabajador();
+        Trabajador trabajador = new Trabajador();
         
-        miTrabajador.setEdad(33);
-        miTrabajador.setNombre("Dorka");
-        miTrabajador.setTelefono("6121876535");
-        miTrabajador.setSalario(50000);
+        trabajador.edad = 33;
+        trabajador.nombre = "Dorka";
+        trabajador.telefono = "6121876535";
+        trabajador.salario = 50000;
 
-        System.out.println("Edad: " + miTrabajador.getEdad() + "\nNombre: " + miTrabajador.getNombre() + "\nTeléfono: " + miTrabajador.getTelefono() + "\nCrédito: " + miTrabajador.getSalario());
-
-        Trabajador miTrabajador1 = new Trabajador();
-        
-        miTrabajador1.setEdad(33);
-        miTrabajador1.setNombre("Dorka");
-        miTrabajador1.setTelefono("6121876535");
-        miTrabajador1.setSalario(50000);
-
-        System.out.println("Edad: " + miTrabajador1.getEdad() + "\nNombre: " + miTrabajador1.getNombre() + "\nTeléfono: " + miTrabajador1.getTelefono() + "\nCrédito: " + miTrabajador1.getSalario());
+        System.out.println("Nombre: " + trabajador.nombre + "\nEdad: " + trabajador.edad + "\nTeléfono: " + trabajador.telefono + "\nSalario: " + "$" + trabajador.salario);
     }
 }
 
 class Persona{
-    private int edad;
-    private String nombre;
-    private String telefono;
-    
-    public int getEdad() {
-        return edad;
-    }
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-    
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    
-    public String getTelefono() {
-        return telefono;
-    }
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+    int edad;
+    String nombre;
+    String telefono;
 }
 
 class Cliente extends Persona{
-    private int credito;
-
-    public int getCredito() {
-        return credito;
-    }
-    public void setCredito(int credito) {
-        this.credito = credito;
-    }
+    int credito;
 }
 
 class Trabajador extends Persona{
-    private int salario;
-
-    public int getSalario() {
-        return salario;
-    }
-    public void setSalario(int salario) {
-        this.salario = salario;
-    }
+    int salario;
 }
