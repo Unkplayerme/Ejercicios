648781cc3e0fa68a3fcfa9ec982d8d50fcbdce8b
Unkplayerme <unkplayerme@gmail.com>
2022-09-20 08:19:10 -0500

    Correccion de errores

--- Tema 3/com/example/Main.java
+++ Tema 3/com/example/Main.java
@@ -9,10 +9,11 @@ public class Main {
             miCoche.addPuerta();
             miCoche.addPuerta();
             System.out.println("El numero de puertas es: " + miCoche.puertas);
+            suma(77, 210, 320);
         }
         public static int suma(int a, int b, int c){
             int resultado = a + b + c;
-            System.out.println(resultado);
+            System.out.println("Resultado de la suma: " + resultado);
             return (resultado);
         }
 }
